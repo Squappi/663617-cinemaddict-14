28e45600d6c488317add981fefcf73c2d4f250ce
Squappi <69323875+Squappi@users.noreply.github.com>
2021-03-31 14:09:43 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+let path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    watchContentBase: true,
+    contentBase: path.resolve(__dirname, 'public')
+  }
+};
