95a7afbaa29876c2c2f06811d8800515452b2bcb
Squappi <69323875+Squappi@users.noreply.github.com>
2021-05-10 00:45:45 +0300

    m

--- src/view/sort.js
+++ src/view/sort.js
@@ -5,26 +5,32 @@ const createSortList = () => {
   return `<ul class="sort">
   <li><a href="#" class="sort__button" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
   <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
-  <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+  <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
 </ul>`;
 };
 
 export default class SiteMenuSort extends Abstract{
   constructor() {
     super();
+
     this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createSortList();
+    return createSortList(this._sortType);
   }
 
   _sortTypeChangeHandler(evt) {
     if(evt.target.tagName !== 'A') {
       return;
     }
+
     evt.preventDefault();
     this._callback.SortTypeChange(evt.target.dataset.sortType);
+
+    this.getElement().querySelectorAll('.sort__button').forEach((button) => {
+      button.classList.remove('sort__button--active');
+    });
   }
 
   sortTypeChangeHandler(callback) {
